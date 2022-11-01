ec7167d632853a3500848b2bd5208668dad765b7
Nico Albanese <gcalbanese96@gmail.com>
2022-11-01 12:49:40 +0000

    added status to response

--- pages/api/search/website/[website].js
+++ pages/api/search/website/[website].js
@@ -1,26 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const { website, api } = req.query
+  const { website, api } = req.query;
   // console.log(query)
   // console.log(website)
   if (api == process.env.NEXT_PUBLIC_APP_API_KEY) {
-    const results = await searchPipeline(website)
-    let resultObj = {}
+    const results = await searchPipeline(website);
+    let resultObj = {};
     if (results.length > 0) {
-      resultObj = { query: website, count: results.length, results }
+      resultObj = {
+        status: "FOUND",
+        query: website,
+        count: results.length,
+        results,
+      };
     } else {
-      resultObj = {query: website, message: `${website} does not exist in the pipeline, follow the link below to create it.`, url: `https://airtable.com/shrUB5NNy0PGzPjQT?prefill_Website=https://${encodeURI(website)}`}
+      resultObj = {
+        status: "NOT FOUND",
+        query: website,
+        message: `${website} does not exist in the pipeline, follow the link below to create it.`,
+        url: `https://airtable.com/shrUB5NNy0PGzPjQT?prefill_Website=https://${encodeURI(
+          website
+        )}`,
+      };
     }
-    res.status(200).json(resultObj)
+    res.status(200).json(resultObj);
   } else {
-    res.status(401).json({ message: "no api key..." })
+    res.status(401).json({ message: "no api key..." });
   }
 }
 
-
 const searchPipeline = async (domain) => {
-  console.log(domain)
+  console.log(domain);
 
   // set headers for call
 
@@ -42,8 +53,7 @@ const searchPipeline = async (domain) => {
   // const AIRTABLE_URL =
   //   `https://api.airtable.com/v0/apptcOM65nkIWJy1l/Pipeline?filterByFormula=SEARCH("${domain.toLowerCase()}", {domain})`;
 
-  const AIRTABLE_URL =
-    `https://api.airtable.com/v0/apptcOM65nkIWJy1l/Pipeline?filterByFormula=SEARCH("${domain}", lower({Website}))`;
+  const AIRTABLE_URL = `https://api.airtable.com/v0/apptcOM65nkIWJy1l/Pipeline?filterByFormula=SEARCH("${domain}", lower({Website}))`;
 
   const res = await fetch(AIRTABLE_URL, requestOptions);
   // console.log(res)
@@ -57,14 +67,15 @@ const searchPipeline = async (domain) => {
       name: company.fields.Name,
       url: company.fields.Website,
       description: company.fields.Description,
-      fund: company.fields.Fund ? company.fields.Fund[0] : "Fund not set yet...",
+      fund: company.fields.Fund
+        ? company.fields.Fund[0]
+        : "Fund not set yet...",
       status: company.fields.Status ? company.fields.Status : "No status...",
       lastStatusChange: company.fields["Last Status Change"],
-      urlToItem: `https://airtable.com/apptcOM65nkIWJy1l/tblltzjPiwy7gOkKE/${company.id}`
+      urlToItem: `https://airtable.com/apptcOM65nkIWJy1l/tblltzjPiwy7gOkKE/${company.id}`,
       // airtableURL: company.fields
     };
   });
 
-
   return structuredData;
-}
+};
